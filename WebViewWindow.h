d11486bb7eafaef0bd99974436cb57df379e823e
cw wang <cwwang@gmail.com>
2014-11-07 17:14:50 -0500

    init

---
+++ WebViewWindow.h
@@ -0,0 +1,12 @@
+
+#import <Cocoa/Cocoa.h>
+
+@interface WebViewWindow : NSWindow
+{
+   IBOutlet WebView* thisWebView;
+}
+
+- (void)setDrawsBackgroundSettings;
+- (void)setView:(WebView*)view onWindow: (NSWindow*) window;
+
+@end
